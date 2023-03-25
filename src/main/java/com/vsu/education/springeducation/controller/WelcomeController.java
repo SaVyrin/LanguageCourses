bfa24230c07722f4aeb9a0f11aea24291ada868b
SaVyrin <sarajkinaa@gmail.com>
2023-03-25 13:56:48 +0300

    Удалены лишние сущности

--- src/main/java/com/vsu/education/springeducation/controller/WelcomeController.java
+++ src/main/java/com/vsu/education/springeducation/controller/WelcomeController.java
@@ -1,6 +1,5 @@
 package com.vsu.education.springeducation.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
